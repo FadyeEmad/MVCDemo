c2cdd20e7b722978dfa03daabb0cf20729aaec04
fady emad <femad7732@gmail.con>
2024-10-28 21:30:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+        app.UseRouting();
+        #region MyRegion
+        //app.UseEndpoints(endpoints =>
+        //{
+
+        //    endpoints.MapGet("/Home", async context =>
+        //    {
+        //        await context.Response.WriteAsync("You are at Products Page");
+        //    });
+        //    endpoints.MapGet("/Products", async context =>
+        //    {
+        //        await context.Response.WriteAsync("You are at Products Page2");
+        //    });
+        //    // app.MapGet("/Home", () => "Hello World!");
+        //}); 
+        #endregion
+        //app.Run(async (HttpContext) => { await HttpContext.Response.WriteAsync(""); });
+        app.Run();
+        app.MapControllerRoute(
+    name: "default",
+    pattern: "{Controller=Home}/{Action=Index}",
+    defaults: new { Controller = "Home", Action = "Index" });
+       app.MapControllerRoute(
+    name: "default",
+    pattern: "{Controller=Home}/{Action=AboutUs}",
+    defaults: new { Controller = "Home", Action = "AboutUs" }
+);
+
+    }
+}
---
+++ controller/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDemo.controller
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Redirect("/Home/AboutUs");
+        }
+        public string AboutUs()
+        {
+            return "This is Inedx Action" ;
+        }
+    }
+}
